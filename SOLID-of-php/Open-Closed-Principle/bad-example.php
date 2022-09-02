ae2809ba5fec5955144c62aeecfa8b6e75a5d46f
dollypizzle <dolapo.mabawonku@gmail.com>
2022-09-02 08:33:19 +0100

    Added examples

--- SOLID-of-php/Open-Closed-Principle/bad-example.php
+++ SOLID-of-php/Open-Closed-Principle/bad-example.php
@@ -1,45 +1,53 @@
-<?php
+<?php 
 
-class AreaCalculator
-{
-    /**
-     * Calculate the total area of the rectangles.
-     *
-     * @param  Illuminate\Support\Collection  $rectangles
-     * @return float
-     */
-    public function Area($rectangles)
-    {
-        $area = 0;
-        $rectangles->each(function ($rectangle) use (&$area) {
-            $area += ($rectangle->width * $rectangle->height); 
-        });
-        return $area;
+class Rectangle {
+  
+    public $width;
+    public $height;
+    
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
     }
 }
 
-class AreaCalculator2
-{
-    /**
-     * Calculate the total area of the shapes.
-     *
-     * @param  Illuminate\Support\Collection  $shapes
-     * @return float
-     */
-    public function Area($shapes)
-    {
-        $area = 0;
-        $shapes->each(function ($shape) use (&$area) {
-            switch (gettype($shape)) {
-                case "Rectangle":
-                    $area += ($shape->width * $shape->height); 
-                    break;
-                case "Circle":
-                    $area += (M_PI * pow($shape->radius, 2));
-                    break;
+class Square {
+  
+    public $length;
+    
+    public function __construct($length) {
+        $this->length = $length;
+    }
+}
+
+class AreaCalculator {
+  
+    protected $shapes;
+    
+    public function __construct($shapes = array()) {
+        $this->shapes = $shapes;
+    }
+    
+    public function sum() {
+        $area = [];
+        
+        foreach($this->shapes as $shape) {
+            if($shape instanceof Square) {
+                $area[] = pow($shape->length, 2);
+            } else if($shape instanceof Rectangle) {
+                $area[] = $shape->width * $shape->height;
             }
-        });
-        return $area;
+        }
+    
+        return array_sum($area);
     }
 }
 
+
+$shapes = new Rectangle(2,5);
+$area = new AreaCalculator($shapes);
+var_dump($area);
+
+$square = new Square(3);
+$area = new AreaCalculator($square);
+var_dump($area);
--- SOLID-of-php/Open-Closed-Principle/good-example.php
+++ SOLID-of-php/Open-Closed-Principle/good-example.php
@@ -1,62 +1,76 @@
 <?php
 
-abstract class Shape
-{
-    // protected $area;
-
-    // public function __construct($area)
-    // {
-    //     $this->area = $area;
-    // }
-
-    // public function getArea()
-    // {
-    //     return $this->area;
-    // }
-
-    abstract protected function getArea();
-
+interface Shape {
+    public function getArea();
 }
 
-class Rectangle extends Shape
-{
-    public $width;  
-    public $height;
-
-    public function getArea()
-    {
+class Rectangle implements Shape {
+  
+    private $width;
+    private $height;
+    
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+    
+    public function getArea() {
         return $this->width * $this->height;
     }
 }
 
-class Circle extends Shape
-{
-    protected $radius = 5;
-
-    public function getArea()
-    {
-        return pow($this->radius, 2) * M_PI;
+class Circle implements Shape {
+  
+    private $radius;
+    
+    public function __construct($radius) {
+        $this->radius = $radius;
+    }
+    
+    public function getArea() {
+        return pow($this->radius, 2) * pi();
     }
 }
 
-class Triangle extends Shape{
-    protected $base = 4;
-
-    protected $height = 7;
-
-    public function getArea()
-    {
-        return .5 * $this->base * $this->height;
+class Square implements Shape {
+  
+    private $length;
+    
+    public function __construct($length) {
+        $this->length = $length;
     }
-} 
+    
+    public function getArea() {
+        return pow($this->length, 2);
+    }
+}
+
+class AreaCalculator {
+  
+    protected $shapes;
+    
+    public function __construct($shapes = array()) {
+        $this->shapes = $shapes;
+    }
+    
+    public function sum() {
+        $area = [];
+        
+        foreach($this->shapes as $shape) {
+            $area[] = $shape->getArea();
+        }
+    
+        return array_sum($area);
+    }
+}
 
 
-$circle = new Circle;
+$circle = new Circle(2);
 var_dump($circle->getArea());
 
-$rectangle = new Rectangle();
-echo($rectangle->getArea());
+$rectangle = new Rectangle(2,3);
+var_dump($rectangle->getArea());
 
-$triangle = new Triangle;
-var_dump($triangle->getArea());
+$square = new Square(7);
+var_dump($square->getArea());
 
